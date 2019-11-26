46749d035533b4db854b3b49381e9118f891f591
Wellesley Yu <byu@lcs.on.ca>
2019-11-26 11:19:13 -0500

    Finished analysis and output area

--- EnglishOrFrench/ViewController.swift
+++ EnglishOrFrench/ViewController.swift
@@ -27,9 +27,35 @@ class ViewController: UIViewController {
         
         }
         
+     
         
         
+        //Start analyzing characters in the input
+        var sCount = 0
+        var tCount = 0
         
+        for character in input {
+            
+            switch character {
+                
+            case "s","S":
+                sCount += 1
+            case "t", "T":
+                tCount += 1
+                
+            default:
+                break
+            }
+            
+            
+        }
+        
+        //Output the text here
+        if tCount > sCount {
+            outputAnalysis.text = "This is probably English text"
+            else
+            outputAnalysis.text = "This is probably French text"
+        }
         
         
         
