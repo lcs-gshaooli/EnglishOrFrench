9912b5b0bf9e18b060251d0cb4a90264314f6cae
Wellesley Yu <byu@lcs.on.ca>
2019-11-26 11:01:01 -0500

    guard statement

--- EnglishOrFrench/ViewController.swift
+++ EnglishOrFrench/ViewController.swift
@@ -19,6 +19,13 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     @IBAction func analyzeText(_ sender: Any) {
+     
+        // guard if no imput is provide
+        guard let input = inputText.text, input.count > 0 else {
+            outputAnalysis.text = "please enter text to analyze"
+            return
+        
+        }
         
         
         
